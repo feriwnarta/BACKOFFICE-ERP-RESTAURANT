c0fa0e0254f316e5e642a23803d23a71de99f4a8
feriwnarta <feriwnarta26@gmail.com>
2023-03-06 16:57:01 +0700

    update

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -2,8 +2,7 @@
     <!-- Sidebar  -->
     <nav id="sidebar">
         <div class="sidebar-logo d-flex flex-row justify-content-center">
-            <img src="{{asset('img/cahaya_senja_logo.png')}}" alt="logo cahaya senja" srcset="" width="118">
-
+            <img src="{{asset('img/cahaya_senja_logo.png')}}" alt="logo cahaya senja" srcset="" style="max-width: 100%">
         </div>
     </nav>
 
