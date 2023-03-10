4af2b1c1fa8ca2eae02fd018af9701afdd9ebef1
feriwnarta <feriwnarta26@gmail.com>
2023-03-10 08:44:14 +0700

    updarte

--- resources/views/components/button/dropdown-no-icon.blade.php
+++ resources/views/components/button/dropdown-no-icon.blade.php
@@ -1,6 +1,7 @@
 <div class="dropdown">
     <button class="btn btn-dropdown-no-icon dropdown-toggle {{ $class }}" type="button" id=" {{ $id }}" data-bs-toggle="dropdown" aria-expanded="false">
         {{ $text }}
+
     </button>
 
     <ul class="dropdown-menu" aria-labelledby="{{ $id }}">
